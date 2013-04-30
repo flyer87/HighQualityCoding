1fa79aa53fc31ea83429a92b8568d00b1bf9fbb6
Yumer Yumer <y.yumer@gmail.com>
2013-04-30 17:00:25 +0300

    10. Code-Tuning-and-Optimization-Homework - Solved 1,2,3

---
+++ 10. Code-Tuning-and-Optimization-Homework/03. AdvancedMathOperations/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.AdvancedMathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 10. Code-Tuning-and-Optimization-Homework/03. AdvancedMathOperations/SqrtTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.AdvancedMathOperations
+{
+    class SqrtTests
+    {
+        public static void SqrtOfFloat(float startValue, float endValue, float offset)
+        {
+            for (float i = startValue; i <= endValue; i += offset)
+            {
+                Math.Sqrt(i);
+            }
+        }
+
+        public static void SqrtOfDecimal(decimal startValue, decimal endValue, decimal offset)
+        {
+            for (decimal i = startValue; i <= endValue; i += offset)
+            {
+                Math.Sqrt((double)i);
+            }
+        }
+
+        public static void SqrtOfDouble(double startValue, double endValue, double offset)
+        {
+            for (double i = startValue; i <= endValue; i += offset)
+            {
+                Math.Sqrt(i);
+            }
+        }
+
+    }
+}
